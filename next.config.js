9788410b266d10e5b4646f860ec4f9f4550603da
NicsonSUPRA <nicsoncc1234@gmail.com>
2025-10-19 11:28:46 -0300

    merge

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: { unoptimized: true },
-    basePath: "/site-acessivel-nicson",
-    assetPrefix: "/site-acessivel-nicson/",
+    output: "export", // ativa exportação estática
 };
 
-module.exports = nextConfig;
+export default nextConfig;
